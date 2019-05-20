64a9bde1af5a9bb50a2289c874ebac8fdee60ccf
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2019-05-20 13:24:58 +0200

    - fiddle with the examples provided by gams

--- src/test.cc
+++ src/test.cc
@@ -26,7 +26,7 @@
 
  /*
    Use this command to compile the example:
-   cl xp_example1.cpp ../C/api/gdxcc.c -I../C/api
+   g++ ./src/test.cc ./src/api/gdxcc.c
  */
 
 #include <string>
@@ -55,74 +55,40 @@ void ReportIOError(int N, const std::string &msg) {
 	exit(1);
 }
 
-void WriteData(gdxHandle_t PGX, const char *s, const double V) {
-	strcpy(Indx[0], s);
-	Values[GMS_VAL_LEVEL] = V;
-	gdxDataWriteStr(PGX, (const char **)Indx, Values);
-}
-
 int main(int argc, char *argv[]) {
+	// define all variables used
 	gdxHandle_t PGX = NULL;
 	char        Msg[GMS_SSSIZE], Producer[GMS_SSSIZE], Sysdir[GMS_SSSIZE], VarName[GMS_SSSIZE];
 	int         ErrNr;
 	int         VarNr;
+	int         SymNr;
+	int         UniqReqs;
 	int         NrRecs;
 	int         N;
 	int         Dim;
 	int         VarTyp;
 	int         D;
-
-	if (argc < 2 || argc > 3) {
-		cout << "**** xp_Example1: incorrect number of parameters" << endl;
-		exit(1);
-	}
-
-	strcpy(Sysdir, argv[1]);
-	cout << "xp_Example1 using GAMS system directory: " << Sysdir << endl;
+	int         S;
+	// set directory for dynamilk link files
+	strcpy(Sysdir, "dll");
 
 	if (!gdxCreateD(&PGX, Sysdir, Msg, sizeof(Msg))) {
 		cout << "**** Could not load GDX library" << endl << "**** " << Msg << endl;
 		exit(1);
 	}
 
-	gdxGetDLLVersion(PGX, Msg);
-	cout << "Using GDX DLL version: " << Msg << endl;
-
 	GDXSTRINDEXPTRS_INIT(IndxXXX, Indx);
 
-	if (2 == argc) {
-		/* Write demand data */
-		gdxOpenWrite(PGX, "demanddata.gdx", "xp_example1", &ErrNr);
-		if (ErrNr) ReportIOError(ErrNr, "gdxOpenWrite");
-		if (!gdxDataWriteStrStart(PGX, "Demand", "Demand data", 1, GMS_DT_PAR, 0))
-			ReportGDXError(PGX);
-		WriteData(PGX, "New-York", 324.0);
-		WriteData(PGX, "Chicago", 299.0);
-		WriteData(PGX, "Topeka", 274.0);
-		if (!gdxDataWriteDone(PGX)) ReportGDXError(PGX);
-		cout << "Demand data written by example1" << endl;
-	}
-	else {
-		gdxOpenRead(PGX, argv[2], &ErrNr);
-		if (ErrNr) ReportIOError(ErrNr, "gdxOpenRead");
-		gdxFileVersion(PGX, Msg, Producer);
-		cout << "GDX file written using version: " << Msg << endl;
-		cout << "GDX file written by: " << Producer << endl;
-
-		if (!gdxFindSymbol(PGX, "x", &VarNr)) {
-			cout << "**** Could not find variable X" << endl;
-			exit(1);
-		}
-
+	gdxOpenRead(PGX, argv[1], &ErrNr);
+	if (ErrNr) ReportIOError(ErrNr, "gdxOpenRead");
+	
+	gdxSystemInfo(PGX, &SymNr, &UniqReqs);
+	cout << "Symbols in GDX: " << SymNr << ", and " << UniqReqs << " unique records." << endl;
+	
+	for (S = 1; S <= SymNr; S++) {
+	  VarNr = S;
 		gdxSymbolInfo(PGX, VarNr, VarName, &Dim, &VarTyp);
-		if (Dim != 2 || GMS_DT_VAR != VarTyp) {
-			cout << "**** X is not a two dimensional variable: "
-				<< Dim << ":" << VarTyp << endl;
-			exit(1);
-		}
-
 		if (!gdxDataReadStrStart(PGX, VarNr, &NrRecs)) ReportGDXError(PGX);
-		cout << "Variable X has " << NrRecs << " records" << endl;
 		while (gdxDataReadStr(PGX, Indx, Values, &N)) {
 			if (0 == Values[GMS_VAL_LEVEL]) continue; /* skip level 0.0 is default */
 			for (D = 0; D < Dim; D++) cout << (D ? '.' : ' ') << Indx[D];
@@ -131,8 +97,8 @@ int main(int argc, char *argv[]) {
 		cout << "All solution values shown" << endl;
 		gdxDataReadDone(PGX);
 	}
-
-	if (ErrNr == gdxClose(PGX)) ReportIOError(ErrNr, "gdxClose");
+	
+	if ((ErrNr = gdxClose(PGX))) ReportIOError(ErrNr, "gdxClose");
 
 	return 0;
 } /* main */
