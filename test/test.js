f4eab31bcfbca1f8e5b00e6d32dc4fd2dc6375cc
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2019-05-23 15:07:41 +0200

    update test

--- test/test.js
+++ test/test.js
@@ -33,3 +33,19 @@ gdx.read('test/test.gdx', 'Demand')
 }).catch(e => {
   console.error(e)
 })
+
+// should fail since the symbol is not in the gdx
+gdx.read('test/test.gdx', 'NotAvailable')
+.then(data => {
+  //
+}).catch(e => {
+  assert.deepStrictEqual(e.message,"Could not find symbol.");
+})
+
+// should fail since file is not available
+gdx.read('notFound.gdx')
+.then(data => {
+  //
+}).catch(e => {
+  assert.deepStrictEqual(e.message,"Failed to open GDX");
+})
